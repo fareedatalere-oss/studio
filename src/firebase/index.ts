55e763cb051845a0149faca00c9f89834b24838e
Khalid Abdullahi <khaledlere@gmail.com>
2026-02-04 07:27:54 +0000

    Try fixing this error: `Runtime ReferenceError: Cannot access 'firebaseA

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -7,24 +7,20 @@ import { firebaseConfig } from './config';
 
 // Re-export hooks and providers
 export { FirebaseProvider, useFirebase, useFirebaseApp, useFirestore, useAuth } from './provider';
-export { FirebaseClientProvider } from './client-provider';
 export { useUser } from './auth/use-user';
 export { useCollection } from './firestore/use-collection';
 export { useDoc } from './firestore/use-doc';
 
 // Initialize Firebase
-let firebaseApp: FirebaseApp;
-let auth: Auth;
-let firestore: Firestore;
-
 function initializeFirebase() {
+  let firebaseApp: FirebaseApp;
   if (getApps().length === 0) {
     firebaseApp = initializeApp(firebaseConfig);
   } else {
     firebaseApp = getApp();
   }
-  auth = getAuth(firebaseApp);
-  firestore = getFirestore(firebaseApp);
+  const auth = getAuth(firebaseApp);
+  const firestore = getFirestore(firebaseApp);
 
   return { firebaseApp, auth, firestore };
 }
