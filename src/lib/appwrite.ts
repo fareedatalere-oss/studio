bc1feed145e63a04508dd9aee82cc14371185240
Khalid Abdullahi <khaledlere@gmail.com>
2026-02-06 16:37:28 +0000

    I forget to tell you isn't I-pay-db I wrote I write profiles please corr

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import { Client, Account, Databases } from 'appwrite';
 
 const client = new Client();
@@ -7,10 +9,9 @@ client
     .setProject('698462e0002b93bc85d9');
 
 export const account = new Account(client);
-// For Appwrite SDK v14+, the database ID is passed to methods, not the constructor.
 export const databases = new Databases(client);
 
-// Per your instruction, the database name/ID is 'profiles'.
+// Per your instruction, the database ID is 'profiles'.
 export const DATABASE_ID = 'profiles';
 
 // Collection IDs
